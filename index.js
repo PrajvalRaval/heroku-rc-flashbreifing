b72c3c8a82b779c1100704ebc2d88a4b61161de4
Prajval Raval <41849970+PrajvalRaval@users.noreply.github.com>
2019-06-24 17:41:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,29 +5,20 @@ const app = express();
 
 app.use(express.json());
 
-//var speechText;
-//var date;
-//var _id;
+var result;
 
 const flashBriefingMessage = async () => {
   await axios
     .get(`https://bots.rocket.chat/api/v1/channels.anonymousread?roomName=flashbriefingchannel`)
     .then((res) => {
-    
-    {
-      uid: res.data.messages[0]._id,
-      updateDate: res.data.messages[0].ts,
-      titleText: "RC FLASH BRIEFING",
-      mainText: res.data.messages[0].msg,
-      redirectionUrl: "COMING SOON"
-    }
 
-      /*speechText = res.data.messages[0].msg;
-      date = res.data.messages[0].ts;
-      _id = res.data.messages[0]._id;
-      console.log(res.data.messages[0].msg);
-      console.log(res.data.messages[0].ts);
-      console.log(res.data.messages[0]._id);*/
+      result = JSON.stringify({
+        uid: res.data.messages[0]._id,
+        updateDate: res.data.messages[0].ts,
+        titleText: "RC FLASH BRIEFING",
+        mainText: res.data.messages[0].msg,
+        redirectionUrl: "COMING SOON"
+      });
 
     })
     .catch((err) => {
@@ -35,19 +26,11 @@ const flashBriefingMessage = async () => {
     });
 };
 
-/*const courses = {
-  uid: _id,
-  updateDate: date,
-  titleText: "RC FLASH BRIEFING",
-  mainText: speechText,
-  redirectionUrl: "COMING SOON"
-};*/
-
 
 app.get('/', async (req, res) => {
   try {
-    const courses = await flashBriefingMessage();
-    res.send(courses);
+    await flashBriefingMessage();
+    res.send(result);
   } catch {
     //this will eventually be handled by your error handling middleware
     res.send(err.message);
