95f7bb6fcf91c20c0bd2aef4c1284c1bd0764a38
Prajval Raval <41849970+PrajvalRaval@users.noreply.github.com>
2019-06-24 19:11:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const PORT = process.env.PORT || 3000;
 const express = require("express");
 const axios = require('axios');
 const app = express();
 
+// Environment Variables
+
+const PORT = process.env.PORT || 3000;
 var client = require('redis').createClient(process.env.REDIS_URL);
+
+// Environment Variables
+
+const serverurl = process.env.SERVER_URL;
+const channelName = process.env.CHANNEL_NAME;
+const flashBriefingTitle = process.env.TITLE_TEXT;
+const cacheTimeout = process.env.CACHE_TTL;
+
 
 client.on('error', (err) => {
   console.log("Error " + err);
@@ -20,18 +30,18 @@ app.get('/', (req, res) => {
       const resultJSON = JSON.parse(result);
       return res.status(200).json(resultJSON);
     } else {
-      return axios.get(`https://bots.rocket.chat/api/v1/channels.anonymousread?roomName=flashbriefingchannel`)
+      return axios.get(`${ serverurl }/api/v1/channels.anonymousread?roomName=${ channelName }`)
         .then(response => {
 
           const responseJSON = JSON.stringify({
             uid: response.data.messages[0]._id,
             updateDate: response.data.messages[0].ts,
-            titleText: "RC FLASH BRIEFING",
+            titleText: flashBriefingTitle,
             mainText: response.data.messages[0].msg,
-            redirectionUrl: "https://bots.rocket.chat/channel/flashbriefingchannel"
+            redirectionUrl: `${ serverurl }/channel/${ channelName }`
           });
 
-          client.setex(`redisdb:message`, 86400, responseJSON);
+          client.setex(`redisdb:message`, cacheTimeout, responseJSON);
           
           const result = JSON.parse(responseJSON);
           return res.status(200).json(result);
